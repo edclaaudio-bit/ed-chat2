338014c32a6b6b67684db0f4d55980d90accda0b
edclaaudio-bit <ed.claaudio@gmail.com>
2025-11-25 12:10:32 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,11 @@ export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>ED Chat</h1>
-      <p>Sistema conectado! Acesse /login para entrar.</p>
+      <p>Você está conectado ao sistema.</p>
+
+      <a href="/login" style={{ fontSize: 20, color: "blue" }}>
+        Entrar no sistema →
+      </a>
     </main>
   );
 }
